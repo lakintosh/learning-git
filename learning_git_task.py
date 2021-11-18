10eba90d2159cfa9d07dba0899b1ec8b7c835740
Krystian Lakatosz <lakatosz.krystian@outlook.com>
2021-11-18 14:06:19 +0100

    Define dictionary

--- learning_git_task.py
+++ learning_git_task.py
@@ -1,1 +1,12 @@
 
+zakupy = {
+    "Netto": ["Woda", "Ziemniaki", "Warzywa"],
+    "Żabka": ["Pączki", "Kawa"]
+    }
+
+for key, value in zakupy.items():
+    print("Idę do " + key.upper() + ", kupuję tu następujące rzeczy: " + str(value).upper())
+
+sum = (len(zakupy["Netto"]) + len(zakupy["Żabka"]))
+
+print("W sumie kupuję " + str(sum) + " produktów.")
