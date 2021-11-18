78c4df9d9b8aeb8dc8e6684c8e74c58ab568c7f7
Krystian Lakatosz <lakatosz.krystian@outlook.com>
2021-11-18 14:15:36 +0100

    Add sum of products

--- learning_git_task.py
+++ learning_git_task.py
@@ -7,6 +7,6 @@ zakupy = {
 for key, value in zakupy.items():
     print("Idę do " + key.upper() + ", kupuję tu następujące rzeczy: " + str(value).upper())
 
-#sum = (len(zakupy["Netto"]) + len(zakupy["Żabka"]))
+sum = (len(zakupy["Netto"]) + len(zakupy["Żabka"]))
 
 #print("W sumie kupuję " + str(sum) + " produktów.")
